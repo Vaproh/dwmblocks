054186b56b21fbf08a476f7a2056a933517a957b
vaproh <vaproh@proton.me>
2024-07-06 14:47:01 +0530

    updated

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",		"vap-music",		1,			9},
-	{"",		"vap-forecast",		900,			13},
 	{"",		"vap-memory",		5,			14},
-	{"",		"vap-cpubars",		1,			15},
-	{"",		"vap-nettraf",		1,			16},
+	{"",		"vap-cpu",		1,			15},
 	{"",		"vap-volume",		1,			10},
 	{"",		"vap-clock",		60,			1},
-	{"",		"vap-internet",		5,			4}
+//	{"",		"vap-internet",		5,			4}
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
