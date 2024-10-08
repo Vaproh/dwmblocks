2fb2cfa17cf9de35b0ed5f7c871f91c6c35dde7e
Vaproh <contact@vaproh.xyz>
2024-10-08 10:53:33 +0530

    added stuf

--- blocks.h
+++ blocks.h
@@ -1,1 +1,16 @@
-../../../.dotfiles/.local/src/dwmblocks/blocks.h
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",		"vap-music",		1,			18},
+	{"",		"vap-forecast",		600,		17},
+	{"",		"vap-battery",		1,			16},
+	{"",		"vap-memory",		5,			14},
+	{"",		"vap-cpu",  		1,			15},
+	{"",		"vap-volume",		1,			10},
+	{"",		"vap-clock",		60,			1},
+	{"",		"vap-internet",		5,			4}
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " ";
+static unsigned int delimLen = 5;
